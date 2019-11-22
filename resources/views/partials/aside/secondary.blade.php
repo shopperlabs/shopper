a53ef23d7fab54f13d6a4d322b07e60526a1220f
Arthur Monney <monneylobe@gmail.com>
2019-11-22 14:55:21 +0100

    update secondary ui

--- resources/views/partials/aside/secondary.blade.php
+++ resources/views/partials/aside/secondary.blade.php
@@ -8,25 +8,25 @@
     <div class="aside__secondary-bottom">
 
         <!-- begin:: Aside Menu -->
-        <div id="aside_menu" class="aside-menu  aziko " data-ktmenu-vertical="1" data-ktmenu-scroll="1">
-            <ul class="menu__nav ">
+        <div id="aside_menu" class="aside-menu  aziko" data-ktmenu-vertical="1" data-ktmenu-scroll="1">
+            <ul class="menu__nav">
                 <li class="menu__section menu__section--first">
                     <h4 class="menu__section-text">Main</h4>
                     <i class="menu__section-icon flaticon-more-v2"></i>
                 </li>
-                <li class="menu__item  menu__item--active" aria-haspopup="true"><a href="index.html" class="menu__link "><span class="menu__link-text">{{ __('Dashboard') }}</span></a></li>
-                <li class="menu__section ">
+                <li class="menu__item menu__item--active" aria-haspopup="true"><a href="{{ route('shopper.dashboard') }}" class="menu__link "><span class="menu__link-text">{{ __('Dashboard') }}</span></a></li>
+                <li class="menu__section">
                     <h4 class="menu__section-text">Components</h4>
                     <i class="menu__section-icon flaticon-more-v2"></i>
                 </li>
                 <li class="menu__item  menu__item--submenu" aria-haspopup="true" data-ktmenu-submenu-toggle="hover"><a href="javascript:;" class="menu__link menu__toggle"><span class="menu__link-text">Charts</span><i class="menu__ver-arrow la la-angle-right"></i></a>
-                    <div class="menu__submenu "><span class="menu__arrow"></span>
+                    <div class="menu__submenu"><span class="menu__arrow"></span>
                         <ul class="menu__subnav">
-                            <li class="menu__item  menu__item--parent" aria-haspopup="true"><span class="menu__link"><span class="menu__link-text">Charts</span></span></li>
-                            <li class="menu__item " aria-haspopup="true"><a href="components/charts/flotcharts.html" class="menu__link "><i class="menu__link-bullet menu__link-bullet--dot"><span></span></i><span class="menu__link-text">Flot Charts</span></a></li>
-                            <li class="menu__item " aria-haspopup="true"><a href="components/charts/google-charts.html" class="menu__link "><i class="menu__link-bullet menu__link-bullet--dot"><span></span></i><span class="menu__link-text">Google Charts</span></a></li>
-                            <li class="menu__item " aria-haspopup="true"><a href="components/charts/morris-charts.html" class="menu__link "><i class="menu__link-bullet menu__link-bullet--dot"><span></span></i><span class="menu__link-text">Morris Charts</span></a></li>
-                            <li class="menu__item " aria-haspopup="true"><a href="components/charts/chart-js.html" class="menu__link "><i class="menu__link-bullet menu__link-bullet--dot"><span></span></i><span class="menu__link-text">Chart JS</span></a></li>
+                            <li class="menu__item menu__item--parent" aria-haspopup="true"><span class="menu__link"><span class="menu__link-text">Charts</span></span></li>
+                            <li class="menu__item" aria-haspopup="true"><a href="components/charts/flotcharts.html" class="menu__link "><i class="menu__link-bullet menu__link-bullet--dot"><span></span></i><span class="menu__link-text">Flot Charts</span></a></li>
+                            <li class="menu__item" aria-haspopup="true"><a href="components/charts/google-charts.html" class="menu__link "><i class="menu__link-bullet menu__link-bullet--dot"><span></span></i><span class="menu__link-text">Google Charts</span></a></li>
+                            <li class="menu__item" aria-haspopup="true"><a href="components/charts/morris-charts.html" class="menu__link "><i class="menu__link-bullet menu__link-bullet--dot"><span></span></i><span class="menu__link-text">Morris Charts</span></a></li>
+                            <li class="menu__item" aria-haspopup="true"><a href="components/charts/chart-js.html" class="menu__link "><i class="menu__link-bullet menu__link-bullet--dot"><span></span></i><span class="menu__link-text">Chart JS</span></a></li>
                         </ul>
                     </div>
                 </li>
