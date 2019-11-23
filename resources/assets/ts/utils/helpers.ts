ce81ae0c50146bc3f11ce3ed50130c51a652a84a
Arthur Monney <monneylobe@gmail.com>
2019-11-23 23:10:37 +0100

    Fix errors lint on helpers file

--- resources/assets/ts/utils/helpers.ts
+++ resources/assets/ts/utils/helpers.ts
@@ -4,4 +4,5 @@
  * @version 1.0
  */
 
-export const toCapitalize = (s: string) => s.substr(0, 1).toUpperCase() + s.substr(1).toLowerCase();
+export const toCapitalize = (s: string) =>
+  s.substr(0, 1).toUpperCase() + s.substr(1).toLowerCase();
