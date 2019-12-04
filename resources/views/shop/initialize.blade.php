6c6342f127fe6c603c033bdc6dd9a6aee822c963
Arthur Monney <monneylobe@gmail.com>
2019-12-04 07:23:49 +0100

    Update shop initialization view

--- resources/views/shop/initialize.blade.php
+++ resources/views/shop/initialize.blade.php
@@ -24,6 +24,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700">
     <!--end::Fonts -->
     <link rel="stylesheet" type="text/css" href="{{ mix('/css/shopper.css', 'shopper') }}">
+    @routes
 </head>
 <body class="shop-initialization">
 
