35aeda747a071d9dea00bb58aaa7c4f3f9b901b4
Arthur Monney <monneylobe@gmail.com>
2019-12-09 05:52:58 +0100

    Add user access route

--- routes/backend.php
+++ routes/backend.php
@@ -12,3 +12,6 @@ Route::redirect('/', \Shopper\Framework\Shopper::prefix() . '/dashboard', 301);
 Route::prefix('dashboard')->group(function() {
     Route::get('/', 'DashboardController@index')->name('dashboard');
 });
+Route::prefix('access')->group(function () {
+    Route::get('users', 'UserController@index')->name('users.access');
+});
