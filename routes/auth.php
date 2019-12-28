9f9d0182ee9cd80e50236873a13a47bd39b69634
Arthur Monney <monneylobe@gmail.com>
2019-12-28 01:56:18 +0100

    Add route name and notification on ForgotPasswordController

--- routes/auth.php
+++ routes/auth.php
@@ -14,4 +14,4 @@ Route::post('/login', 'LoginController@login')->name('login');
 Route::get('/password/reset', 'ForgotPasswordController@showLinkRequestForm')->name('password.request');
 Route::post('/password/email', 'ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('/password/reset/{token}', 'ResetPasswordController@showResetForm')->name('password.reset');
-Route::post('/password/reset', 'ResetPasswordController@reset');
+Route::post('/password/reset', 'ResetPasswordController@reset')->name('password.update');
--- src/Http/Controllers/Auth/ForgotPasswordController.php
+++ src/Http/Controllers/Auth/ForgotPasswordController.php
@@ -2,6 +2,7 @@
 
 namespace Shopper\Framework\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Notifications\Messages\MailMessage;
@@ -35,6 +36,7 @@ class ForgotPasswordController extends Controller
 
         ResetPassword::toMailUsing(function ($notifiable, $token) {
             return (new MailMessage)
+                ->view('shopper::mails.email')
                 ->line(__('You are receiving this email because we received a password reset request for your account.'))
                 ->action(__('Reset Password'), url(config('app.url').route('shopper.password.reset', $token, false)))
                 ->line(__('If you did not request a password reset, no further action is required.'));
@@ -49,5 +51,35 @@ class ForgotPasswordController extends Controller
     public function showLinkRequestForm()
     {
         return view('shopper::auth.passwords.email');
+    }
+
+    /**
+     * Get the response for a successful password reset link.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendResetLinkResponse(Request $request, $response)
+    {
+        notify()->emotify('success', trans($response));
+
+        return back();
+    }
+
+    /**
+     * Get the response for a failed password reset link.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendResetLinkFailedResponse(Request $request, $response)
+    {
+        notify()->emotify('error', trans($response));
+
+        return back()
+            ->withInput($request->only('email'))
+            ->withErrors(['email' => trans($response)]);
     }
 }
