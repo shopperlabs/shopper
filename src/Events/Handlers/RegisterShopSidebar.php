c650fd3206d6cb22af14108cb7b218798733f218
Arthur Monney <monneylobe@gmail.com>
2020-03-24 15:09:40 +0100

    :lipstick: Update Shop sidebar

--- src/Events/Handlers/RegisterShopSidebar.php
+++ src/Events/Handlers/RegisterShopSidebar.php
@@ -26,7 +26,7 @@ class RegisterShopSidebar extends AbstractAdminSidebar
                 $item->authorize(true);
                 $item->route('shopper.categories.index');
                 $item->icon('
-                    <svg class="flex-shrink-0 -ml-1 mr-3 h-5 w-5 transition ease-in-out duration-200" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                    <svg class="flex-shrink-0 -ml-1 mr-3 h-5 w-5" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
                     </svg>
                 ');
@@ -37,8 +37,18 @@ class RegisterShopSidebar extends AbstractAdminSidebar
                 $item->authorize(true);
                 $item->route('shopper.brands.index');
                 $item->icon('
-                    <svg class="flex-shrink-0 -ml-1 mr-3 h-5 w-5 transition ease-in-out duration-200" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                    <svg class="flex-shrink-0 -ml-1 mr-3 h-5 w-5" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01" />
+                    </svg>
+                ');
+            });
+
+            $group->item(__('Collections'), function (Item $item) {
+                $item->weight(3);
+                $item->authorize(true);
+                $item->icon('
+                    <svg class="flex-shrink-0 -ml-1 mr-3 h-5 w-5" fill="currentColor" viewBox="0 0 24 24">
+                        <path d="M22.47,18.82l-1-3.86h0L18.32,3.37a1,1,0,0,0-1.22-.71l-3.87,1a1,1,0,0,0-.73-.33H2.5a1,1,0,0,0-1,1v16a1,1,0,0,0,1,1h10a1,1,0,0,0,1-1v-8l2.2,8.22a1,1,0,0,0,1,.74,1.15,1.15,0,0,0,.26,0l4.83-1.29a1,1,0,0,0,.61-.47A1.05,1.05,0,0,0,22.47,18.82Zm-16,.55h-3v-2h3Zm0-4h-3v-6h3Zm0-8h-3v-2h3Zm5,12h-3v-2h3Zm0-4h-3v-6h3Zm0-8h-3v-2h3Zm2.25-1.74,2.9-.78.52,1.93-2.9.78Zm2.59,9.66-1.55-5.8,2.9-.78,1.55,5.8Zm1,3.86-.52-1.93,2.9-.78.52,1.93Z" />
                     </svg>
                 ');
             });
@@ -47,7 +57,7 @@ class RegisterShopSidebar extends AbstractAdminSidebar
                 $item->weight(3);
                 $item->authorize(true);
                 $item->icon('
-                    <svg class="flex-shrink-0 -ml-1 mr-3 h-5 w-5 transition ease-in-out duration-200" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                    <svg class="flex-shrink-0 -ml-1 mr-3 h-5 w-5" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
                     </svg>
                 ');
@@ -57,7 +67,7 @@ class RegisterShopSidebar extends AbstractAdminSidebar
                 $item->weight(4);
                 $item->authorize(true);
                 $item->icon('
-                    <svg class="flex-shrink-0 -ml-1 mr-3 h-5 w-5 transition ease-in-out duration-200" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                    <svg class="flex-shrink-0 -ml-1 mr-3 h-5 w-5" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
                     </svg>
                 ');
@@ -67,7 +77,7 @@ class RegisterShopSidebar extends AbstractAdminSidebar
                 $item->weight(5);
                 $item->authorize(true);
                 $item->icon('
-                    <svg class="flex-shrink-0 -ml-1 mr-3 h-5 w-5 transition ease-in-out duration-200" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                    <svg class="flex-shrink-0 -ml-1 mr-3 h-5 w-5" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
                     </svg>
                 ');
