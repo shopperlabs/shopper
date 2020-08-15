cd2ac3186c2f2d18d158366145d2bb530e4af953
Arthur Monney <monneylobe@gmail.com>
2020-08-15 18:18:56 +0100

    :seedling: Update ShopSize seeder with real size description

--- database/seeds/ShopTableSeeder.php
+++ database/seeds/ShopTableSeeder.php
@@ -18,21 +18,21 @@ class ShopTableSeeder extends Seeder
         ShopSize::create([
             'name' => __('Small'),
             'size' => __('=< 100 Products'),
-            'description' => __('Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.'),
+            'description' => __('You have just opened your store and you do not yet have a wide range of products. Start your activity slowly.'),
             'max_products' => 100
         ]);
 
         ShopSize::create([
             'name' => __('Medium'),
             'size' => __('>= 101 & <= 1000 Products'),
-            'description' => __('Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.'),
+            'description' => __('You have a store that expands with a lot of product varieties, start with this plan and fill out all of your products.'),
             'max_products' => 1000
         ]);
 
         ShopSize::create([
             'name' => __('Bigger'),
             'size' => __('>= 1001 Products'),
-            'description' => __('Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.'),
+            'description' => __('You have a business with employees that sells products and you want to boost your business.'),
             'max_products' => 10000
         ]);
 
