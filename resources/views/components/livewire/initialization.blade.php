ce7fb21d9964f2d98e6fa2d54abdaa019560cdd7
Arthur Monney <monneylobe@gmail.com>
2020-08-24 15:18:29 +0100

    :bento: Update Tailwind

--- resources/views/components/livewire/initialization.blade.php
+++ resources/views/components/livewire/initialization.blade.php
@@ -388,9 +388,10 @@
                     <div class="flex justify-end">
                         <span class="ml-3 inline-flex rounded-md shadow-sm">
                             <button wire:click="store" type="button" class="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition duration-150 ease-in-out">
-                                <span wire:loading wire:target="store" class="pr-2">
-                                    <span class="btn-spinner"></span>
-                                </span>
+                                <svg wire:loading wire:target="store" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" fill="none" viewBox="0 0 24 24">
+                                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"/>
+                                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"/>
+                                </svg>
                                 {{ __("Create my shop") }}
                             </button>
                         </span>
