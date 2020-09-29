c07beee63c2f2d0aa4bf75dd11fa278eb557e36c
Arthur Monney <monneylobe@gmail.com>
2020-09-29 09:20:26 +0100

    :lipstick: Update dashboard view

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -106,7 +106,7 @@
                 </div>
             </div>
 
-            <div class="p-6 border-t border-gray-200">
+            <div class="px-6 py-5 border-t border-gray-200">
                 <div class="flex items-center">
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6 text-blue-600">
                         <path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
@@ -114,7 +114,7 @@
                     <h4 class="ml-4 text-base text-gray-900 leading-6 font-medium">Add products</h4>
                 </div>
 
-                <div class="ml-12">
+                <div class="ml-10">
                     <div class="mt-2 text-sm text-gray-500">
                         Add products and prices to start selling. Tailor it to your store's needs with an unlimited number of products (depending on the size of your store), brands, collections, and variations.
                     </div>
--- resources/views/pages/layout.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('shopper::layouts.'. config('shopper.theme'))
-
-@section('content')
-
-    <h1>Demo</h1>
-
-@endsection
