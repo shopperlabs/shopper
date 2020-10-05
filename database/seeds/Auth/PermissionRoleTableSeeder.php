ef8b50ebb86a7f59e01427419c33952e2bc08a71
Arthur Monney <monneylobe@gmail.com>
2020-10-05 05:09:26 +0100

    :art: Update permission class seeder

--- database/seeds/Auth/PermissionRoleTableSeeder.php
+++ database/seeds/Auth/PermissionRoleTableSeeder.php
@@ -17,21 +17,15 @@ class PermissionRoleTableSeeder extends Seeder
 
         // Create Roles
         Role::create([
-            'name' => config('shopper.users.admin_role'),
+            'name' => config('shopper.config.users.admin_role'),
             'display_name' => 'Administrator',
             'description' => 'Site administrator with access to shop and developer tools.'
         ]);
 
         Role::create([
-            'name' => 'seller',
-            'display_name' => 'Seller',
-            'description' => 'Merchant of the site with the access for the publications of its products'
-        ]);
-
-        Role::create([
-            'name' => config('shopper.users.default_role'),
+            'name' => config('shopper.config.users.default_role'),
             'display_name' => 'User',
-            'description' => 'Site customer role with acces on front site.'
+            'description' => 'Site customer role with access on front site.'
         ]);
 
         $this->enableForeignKeys();
