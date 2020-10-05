8f2a2c2a1de97ce606451eb237f8cf686704d302
Jean-Vincent QUILICHINI <jv.quilichini@jellysmack.com>
2020-10-05 16:11:30 +0200

    🚧 Updated some models

---
+++ src/Models/Shop/Carrier.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Shopper\Framework\Models\Shop;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Carrier extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'is_enabled' => 'boolean',
+    ];
+
+    /**
+     * Boot the model.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($model) {
+            $model->update(['slug' => $model->name]);
+        });
+    }
+
+    /**
+     * Get the table associated with the model.
+     *
+     * @return string
+     */
+    public function getTable()
+    {
+        return shopper_table('carriers');
+    }
+
+    /**
+     * Set the proper slug attribute.
+     *
+     * @param  string  $value
+     */
+    public function setSlugAttribute($value)
+    {
+        if (static::query()->where('slug', $slug = str_slug($value))->exists()) {
+            $slug = "{$slug}-{$this->id}";
+        }
+
+        $this->attributes['slug'] = $slug;
+    }
+}
