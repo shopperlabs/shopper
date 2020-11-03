672d1bb11b18c6c91f03ab00df342ef7ac1a6f1b
Arthur Monney <monneylobe@gmail.com>
2020-11-03 17:52:21 +0100

    :recycle: format categories browse view

--- resources/views/livewire/categories/browse.blade.php
+++ resources/views/livewire/categories/browse.blade.php
@@ -221,7 +221,7 @@
                                     <td colspan="5" class="px-6 py-3 whitespace-no-wrap text-sm leading-5 font-medium text-gray-900">
                                         <div class="flex justify-center items-center space-x-2">
                                             <svg class="h-8 w-8 text-cool-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z" />
                                             </svg>
                                             <span class="font-medium py-8 text-cool-gray-400 text-xl">{{ __("No category found") }}...</span>
                                         </div>
