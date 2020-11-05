32df699c17a90224edfd85144c5e95b92dd8c02f
Arthur Monney <monneylobe@gmail.com>
2020-11-05 01:42:16 +0100

    :lipstick: Create modal component

---
+++ resources/views/components/modal.blade.php
@@ -0,0 +1,67 @@
+@props(['model', 'maxWidth' => null])
+
+@php
+    switch ($maxWidth ?? '2xl') {
+        case 'sm':
+            $maxWidth = 'sm:max-w-sm';
+            break;
+        case 'md':
+            $maxWidth = 'sm:max-w-md';
+            break;
+        case 'lg':
+            $maxWidth = 'sm:max-w-lg';
+            break;
+        case 'xl':
+            $maxWidth = 'sm:max-w-xl';
+            break;
+        case '2xl':
+        default:
+            $maxWidth = 'sm:max-w-2xl';
+            break;
+    }
+@endphp
+
+<div
+    x-cloak
+    {{ $attributes }}
+    class="fixed z-50 inset-0 overflow-y-auto"
+    x-on:close.stop="show = false"
+    x-on:keydown.escape.window="show = false"
+>
+    <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div
+            x-cloak
+            x-show="show"
+            x-description="Background overlay, show/hide based on modal state."
+            x-transition:enter="ease-out duration-300"
+            x-transition:enter-start="opacity-0"
+            x-transition:enter-end="opacity-100"
+            x-transition:leave="ease-in duration-200"
+            x-transition:leave-start="opacity-100"
+            x-transition:leave-end="opacity-0"
+            class="fixed inset-0 transition-opacity"
+        >
+            <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+        </div>
+
+        <!-- This element is to trick the browser into centering the modal contents. -->
+        <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>
+        <div
+            x-cloak
+            x-show="show"
+            x-description="Modal panel, show/hide based on modal state."
+            x-transition:enter="ease-out duration-300"
+            x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
+            x-transition:leave="ease-in duration-200"
+            x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
+            x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle {{ $maxWidth }} sm:w-full"
+            role="dialog"
+            aria-modal="true"
+            aria-labelledby="modal-headline"
+        >
+            {{ $slot }}
+        </div>
+    </div>
+</div>
