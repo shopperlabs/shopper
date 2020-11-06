b5d4010d9a54f6f549eb4f162ae9349380b42ccd
Arthur Monney <monneylobe@gmail.com>
2020-11-06 06:00:41 +0100

    :white_check_mark: Enable unit test

---
+++ tests/ShopperTestCase.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Shopper\Framework\Tests;
+
+use Livewire\LivewireServiceProvider;
+use Mockery;
+use Orchestra\Testbench\TestCase;
+use Shopper\Framework\FrameworkServiceProvider;
+use Shopper\Framework\Models\User\User;
+
+class ShopperTestCase extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function tearDown(): void
+    {
+        Mockery::close();
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [LivewireServiceProvider::class, FrameworkServiceProvider::class];
+    }
+
+    public function getEnvironmentSetUp($app)
+    {
+        $app['migrator']->path(__DIR__.'/../database/migrations');
+
+        $app['config']->set('database.default', 'sqlite');
+
+        $app['config']->set('database.connections.sqlite', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+
+        // Use test User model for users provider
+        $app['config']->set('auth.providers.users.model', User::class);
+    }
+}
