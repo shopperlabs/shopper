b81f9805159d155aed87a0ed119d5ab2a856e89b
Arthur Monney <monneylobe@gmail.com>
2020-11-14 02:03:58 +0100

    :sparkles: add permission checked

--- resources/views/livewire/collections/browse.blade.php
+++ resources/views/livewire/collections/browse.blade.php
@@ -3,13 +3,15 @@
     <div class="mt-4 pb-5 border-b border-gray-200 space-y-3 sm:flex sm:items-center sm:justify-between sm:space-x-4 sm:space-y-0">
         <h2 class="text-2xl font-bold leading-6 text-gray-900 sm:text-3xl sm:leading-9 sm:truncate">{{ __('Collections') }}</h2>
         @if($total > 0)
-            <div class="flex space-x-3">
-                <span class="shadow-sm rounded-md">
-                    <a href="{{ route('shopper.collections.create') }}" class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-700 active:bg-blue-700 transition duration-150 ease-in-out">
-                        {{ __("Create") }}
-                    </a>
-                </span>
-            </div>
+            @can('add_collections')
+                <div class="flex space-x-3">
+                    <span class="shadow-sm rounded-md">
+                        <a href="{{ route('shopper.collections.create') }}" class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-700 active:bg-blue-700 transition duration-150 ease-in-out">
+                            {{ __("Create") }}
+                        </a>
+                    </span>
+                </div>
+            @endcan
         @endif
     </div>
 
@@ -18,6 +20,7 @@
             :title="__('Organize your products into collection categories')"
             :content="__('Create and manage all your collections to help your customers easily find products.')"
             :button="__('Create collection')"
+            permission="add_collections"
             :url="route('shopper.collections.create')"
         >
             <div class="flex-shrink-0">
--- resources/views/livewire/customers/browse.blade.php
+++ resources/views/livewire/customers/browse.blade.php
@@ -3,13 +3,15 @@
     <div class="mt-4 pb-5 border-b border-gray-200 space-y-3 sm:flex sm:items-center sm:justify-between sm:space-x-4 sm:space-y-0">
         <h2 class="text-2xl font-bold leading-6 text-gray-900 sm:text-3xl sm:leading-9 sm:truncate">{{ __('Customers') }}</h2>
         @if($total > 0)
-            <div class="flex space-x-3">
-                <span class="shadow-sm rounded-md">
-                    <a href="{{ route('shopper.customers.create') }}" class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-700 active:bg-blue-700 transition duration-150 ease-in-out">
-                        {{ __("Create") }}
-                    </a>
-                </span>
-            </div>
+            @can('add_customers')
+                <div class="flex space-x-3">
+                    <span class="shadow-sm rounded-md">
+                        <a href="{{ route('shopper.customers.create') }}" class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-700 active:bg-blue-700 transition duration-150 ease-in-out">
+                            {{ __("Create") }}
+                        </a>
+                    </span>
+                </div>
+            @endcan
         @endif
     </div>
 
@@ -18,6 +20,7 @@
             :title="__('Manage customer orders & details')"
             :content="__('This is where you can manage your customer information and view their purchase history.')"
             :button="__('Add customer')"
+            permission="add_customers"
             :url="route('shopper.customers.create')"
         >
             <div class="flex-shrink-0">
