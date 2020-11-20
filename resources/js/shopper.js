0b1faa61fa42425264917c008fa0f0064060a8bd
Arthur Monney <monneylobe@gmail.com>
2020-11-20 14:22:37 +0100

    :bento: Add toggle dark mode action

--- resources/js/shopper.js
+++ resources/js/shopper.js
@@ -20,6 +20,26 @@ if (document.getElementById('setting-configuration')) {
   require("@/src/pages/Settings/Configuration");
 }
 
+function toggleDarkMode() {
+  // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+  if (localStorage.theme === 'dark' || (!'theme' in localStorage && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.querySelector('html').classList.add('dark');
+  } else if (localStorage.theme === 'light') {
+    document.querySelector('html').classList.add('light');
+  }
+
+// Whenever the user explicitly chooses light mode
+  localStorage.theme = 'light';
+
+// Whenever the user explicitly chooses dark mode
+  localStorage.theme = 'dark';
+
+// Whenever the user explicitly chooses to respect the OS preference
+  localStorage.removeItem('theme');
+}
+
+toggleDarkMode();
+
 /**
  * Custom HTML script code.
  * @type {HTMLElement}
