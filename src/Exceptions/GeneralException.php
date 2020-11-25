f5c0220f029728ed8b2e59ce50acba71ca8be577
Arthur Monney <monneylobe@gmail.com>
2020-11-25 23:56:04 +0100

    :goal_net: Update Laravel Shopper Catching error views

--- src/Exceptions/GeneralException.php
+++ src/Exceptions/GeneralException.php
@@ -3,29 +3,13 @@
 namespace Shopper\Framework\Exceptions;
 
 use Exception;
-use Throwable;
 
 class GeneralException extends Exception
 {
     /**
-     * @var
-     */
-    public $message;
-
-    /**
-     * GeneralException constructor.
-     *
-     * @param string         $message
-     * @param int            $code
-     * @param Throwable|null $previous
-     */
-    public function __construct($message = '', $code = 0, Throwable $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
-
-    /**
      * Report the exception.
+     *
+     * @return void
      */
     public function report()
     {
@@ -36,14 +20,13 @@ class GeneralException extends Exception
      * Render the exception into an HTTP response.
      *
      * @param  \Illuminate\Http\Request
-     * @return \Illuminate\Http\RedirectResponse
+     * @return \Illuminate\Http\Response
      */
     public function render($request)
     {
-        // All instances of GeneralException redirect back with a flash message to show a bootstrap alert-error
-        return redirect()
-            ->back()
-            ->withInput()
-            ->with('danger', $this->message);
+        return response()->view(
+            'shopper::errors.template',
+            ['exception' => $this]
+        );
     }
 }
