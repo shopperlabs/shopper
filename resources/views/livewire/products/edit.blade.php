bd9b09ed71813100d31ab3fe03f95cefd3726622
Arthur Monney <monneylobe@gmail.com>
2020-12-04 15:53:29 +0100

    :construction: wip attributes products

--- resources/views/livewire/products/edit.blade.php
+++ resources/views/livewire/products/edit.blade.php
@@ -134,8 +134,8 @@
         <div x-cloak x-show="currentTab === 'variants'" class="bg-white overflow-hidden rounded-lg shadow-md">
             <x-shopper-wip-placeholder />
         </div>
-        <div x-cloak x-show="currentTab === 'attributes'" class="bg-white overflow-hidden rounded-lg shadow-md">
-            <x-shopper-wip-placeholder />
+        <div x-cloak x-show="currentTab === 'attributes'">
+            <livewire:shopper-products-form-attributes :product="$product" />
         </div>
         <div x-cloak x-show="currentTab === 'inventory'">
             <livewire:shopper-products-form-inventory :product="$product" />
@@ -151,44 +151,8 @@
         </div>
     </div>
 
-    <div
-        x-cloak
-        x-show="show"
-        class="fixed z-50 inset-0 overflow-y-auto flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
-    >
-        <div
-            x-cloak
-            x-show="show"
-            x-description="Background overlay, show/hide based on modal state."
-            x-transition:enter="ease-out duration-300"
-            x-transition:enter-start="opacity-0"
-            x-transition:enter-end="opacity-100"
-            x-transition:leave="ease-in duration-200"
-            x-transition:leave-start="opacity-100"
-            x-transition:leave-end="opacity-0"
-            class="fixed inset-0 transition-opacity"
-        >
-            <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
-        </div>
-
-        <!-- This element is to trick the browser into centering the modal contents. -->
-        <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>
-        <div
-            x-cloak
-            x-show="show"
-            x-description="Modal panel, show/hide based on modal state."
-            x-transition:enter="ease-out duration-300"
-            x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
-            x-transition:leave="ease-in duration-200"
-            x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
-            x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6"
-            role="dialog"
-            aria-modal="true"
-            aria-labelledby="modal-headline"
-            style="display: none;"
-        >
+    <x-shopper-modal x-show="show" maxWidth="lg">
+        <div class="bg-white rounded-lg px-4 pt-5 pb-4 text-left">
             <div class="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
                 <button @click="show = false;" type="button" class="text-gray-400 hover:text-gray-500 focus:outline-none focus:text-gray-500 transition ease-in-out duration-150" aria-label="Close">
                     <svg class="h-6 w-6" x-description="Heroicon name: x" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -230,5 +194,5 @@
                 </span>
             </div>
         </div>
-    </div>
+    </x-shopper-modal>
 </div>
--- src/Http/Livewire/Products/Form/Attributes.php
+++ src/Http/Livewire/Products/Form/Attributes.php
@@ -7,6 +7,23 @@ use Livewire\Component;
 class Attributes extends Component
 {
     /**
+     * Product Model.
+     *
+     * @var \Illuminate\Database\Eloquent\Model
+     */
+    public $product;
+
+    /**
+     * Component Mount method.
+     *
+     * @return void
+     */
+    public function mount($product)
+    {
+        $this->product = $product;
+    }
+
+    /**
      * Render the component.
      *
      * @return \Illuminate\View\View
