63fd00c0c31c0af258f70b1be7fb97f683487e37
Arthur Monney <monneylobe@gmail.com>
2020-12-17 06:38:28 +0100

    :lipstick: Update component and add label blade component

--- resources/views/components/input/textarea.blade.php
+++ resources/views/components/input/textarea.blade.php
@@ -1,3 +1,1 @@
-<div class="flex rounded-md shadow-sm">
-    <textarea {{ $attributes }} rows="3" class="form-textarea block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"></textarea>
-</div>
+<textarea {{ $attributes }} rows="4" class="form-textarea block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"></textarea>
