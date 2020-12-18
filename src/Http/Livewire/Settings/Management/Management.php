0b19afe5e6503593834a02bc451968df8b8b9a09
Arthur Monney <monneylobe@gmail.com>
2020-12-18 10:12:20 +0100

    :recycle: Update Management component

--- src/Http/Livewire/Settings/Management/Management.php
+++ src/Http/Livewire/Settings/Management/Management.php
@@ -3,6 +3,7 @@
 namespace Shopper\Framework\Http\Livewire\Settings\Management;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\View\View;
 use Livewire\Component;
 use Livewire\WithPagination;
 use Shopper\Framework\Models\User\Role;
@@ -12,17 +13,46 @@ class Management extends Component
 {
     use WithPagination;
 
-    public string $name = '';
-    public string $role_display_name = '';
-    public string $role_description = '';
+    /**
+     * Role name.
+     *
+     * @var string
+     */
+    public $name;
 
+    /**
+     * Role display name.
+     *
+     * @var string
+     */
+    public $role_display_name;
+
+    /**
+     * Role description.
+     *
+     * @var string
+     */
+    public $role_description;
+
+    /**
+     * Toggle Modal to create a new role.
+     *
+     * @var bool
+     */
+    public $displayModal = false;
+
+    /**
+     * Add new role.
+     *
+     * @return void
+     */
     public function addNewRole()
     {
         $this->validate([
             'name' => 'required|unique:roles',
         ], [
-            'name.required' => 'The role name is required.',
-            'name.unique' => 'This name is already used.'
+            'name.required' => __('The role name is required.'),
+            'name.unique' => __('This name is already used.')
         ]);
 
         Role::create([
@@ -31,9 +61,10 @@ class Management extends Component
             'description' => $this->role_description,
         ]);
 
-        $this->dispatchBrowserEvent('role-added');
-        $this->dispatchBrowserEvent('notify', [
-            'title' => 'Saved',
+        $this->dispatchBrowserEvent('modal-close');
+        $this->displayModal = false;
+        $this->notify([
+            'title' => __('Saved'),
             'message' => __("Role saved successfully"),
         ]);
 
@@ -42,33 +73,55 @@ class Management extends Component
         $this->role_description = '';
     }
 
+    /**
+     * Action provide to launch the modal.
+     *
+     * @return void
+     */
+    public function launchCreateModal()
+    {
+        $this->displayModal = true;
+    }
+
+    /**
+     * Remove a user from the storage.
+     *
+     * @param  int  $id
+     * @throws \Exception
+     */
     public function removeUser(int $id)
     {
         (new UserRepository())->getById($id)->delete();
+
         $this->dispatchBrowserEvent('user-removed');
-        $this->dispatchBrowserEvent('notify', [
+        $this->notify([
             'title' => __('Deleted'),
             'message' => __("Admin deleted successfully"),
         ]);
     }
 
+    /**
+     * Render the component.
+     *
+     * @return \Illuminate\View\View
+     * @throws \Shopper\Framework\Exceptions\GeneralException
+     */
     public function render()
     {
-        $roles = Role::query()
-            ->with('users')
-            ->where('name', '<>', config('shopper.system.users.default_role'))
-            ->limit(3)
-            ->orderBy('created_at')
-            ->get();
-
-        $users = (new UserRepository())
-            ->makeModel()
-            ->whereHas('roles', function (Builder $query) {
-                $query->where('name', '<>', config('shopper.system.users.default_role'));
-            })
-            ->orderBy('created_at', 'desc')
-            ->paginate(3);
-
-        return view('shopper::livewire.settings.management.index', compact('roles', 'users'));
+        return view('shopper::livewire.settings.management.index', [
+            'roles' => Role::query()
+                ->with('users')
+                ->where('name', '<>', config('shopper.system.users.default_role'))
+                ->limit(3)
+                ->orderBy('created_at')
+                ->get(),
+            'users' => (new UserRepository())
+                ->makeModel()
+                ->whereHas('roles', function (Builder $query) {
+                    $query->where('name', '<>', config('shopper.system.users.default_role'));
+                })
+                ->orderBy('created_at', 'desc')
+                ->paginate(3),
+        ]);
     }
 }
