c639a92b1a3f69c343bb312e6430a1aebbbb8c66
Arthur Monney <monneylobe@gmail.com>
2021-01-18 21:08:00 +0100

    :card_file_box: Create migration, model and seeder for legal pages

---
+++ database/migrations/2021_01_18_174504_create_legals_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Shopper\Framework\Traits\Database\MigrationTrait;
+
+class CreateLegalsTable extends Migration
+{
+    use MigrationTrait;
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->getTableName('legals'), function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->string('slug')->nullable()->unique();
+            $table->longText('content')->nullable();
+            $table->boolean('is_enabled')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->getTableName('legals'));
+    }
+}
