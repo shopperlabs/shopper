225f879e341fb6e5941752cb2fdc08e9734703a4
Arthur Monney <monneylobe@gmail.com>
2021-02-11 04:53:58 +0100

    :recycle: Update placeholder on social links

--- resources/js/src/pages/Settings/Configuration.jsx
+++ resources/js/src/pages/Settings/Configuration.jsx
@@ -583,7 +583,7 @@ const Configuration = () => {
                         value={values.shop_facebook_link}
                         onChange={handleChange}
                         className="form-input pl-10 block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"
-                        placeholder="https://facebook.com/mckenziearts"
+                        placeholder="username"
                         autoComplete="off"
                       />
                     </div>
@@ -603,7 +603,7 @@ const Configuration = () => {
                         value={values.shop_instagram_link}
                         onChange={handleChange}
                         className="form-input pl-10 block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"
-                        placeholder="@mckenziearts"
+                        placeholder="username"
                         autoComplete="off"
                       />
                     </div>
@@ -623,7 +623,7 @@ const Configuration = () => {
                         value={values.shop_twitter_link}
                         onChange={handleChange}
                         className="form-input pl-10 block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"
-                        placeholder="@mckenziearts"
+                        placeholder="username"
                         autoComplete="off"
                       />
                     </div>
