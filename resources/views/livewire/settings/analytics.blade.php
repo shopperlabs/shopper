fcbbd6ffa59fe4488560ad946dbf1bb4cacc8f52
Arthur Monney <monneylobe@gmail.com>
2021-02-20 14:28:13 +0100

    :recycle: Update analytics setting, adding json upload file

--- resources/views/livewire/settings/analytics.blade.php
+++ resources/views/livewire/settings/analytics.blade.php
@@ -52,23 +52,33 @@
 
                         <div class="mt-6">
                             <label class="inline-flex items-center block text-sm leading-5 font-medium text-gray-700">
-                                {{ __('Live Reporting') }}
-                                <button type="button" class="ml-3 text-gray-400 hover:text-gray-500 outline-none focus:outline-none leading-4 transition duration-200 ease-in-out">
+                                {{ __('Json Account Credentials') }}
+                                <a href="https://github.com/spatie/laravel-analytics#how-to-obtain-the-credentials-to-communicate-with-google-analytics" target="_blank" class="ml-3 text-gray-400 hover:text-gray-500 outline-none focus:outline-none leading-4 transition duration-200 ease-in-out">
                                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
                                     </svg>
-                                </button>
+                                </a>
                             </label>
                             <div class="mt-2 flex items-center">
-                                <span class="rounded-md shadow-sm">
-                                    <button type="button" class="inline-flex items-center py-2 px-3 border border-gray-300 rounded-md text-sm leading-4 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out">
-                                        <svg class="h-5 w-5 mr-1.5" fill="none" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M2.667 10.667v.666a2 2 0 0 0 2 2h6.666a2 2 0 0 0 2-2v-.666M10.667 8L8 10.667m0 0L5.333 8M8 10.667v-8" stroke="#374151" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                                        </svg>
-                                        {{ __('Upload') }}
-                                    </button>
-                                </span>
-                                <span class="ml-4 text-sm leading-5 text-gray-400">{{ __("No json file selected.") }}</span>
+                                <div x-data="{ focused: false }">
+                                    <span>
+                                        <input @focus="focused = true" @blur="focused = false" class="sr-only" type="file" id="json_file" wire:model="json_file">
+                                        <label for="json_file" :class="{ 'outline-none border-blue-300 shadow-outline-blue': focused }" class="cursor-pointer inline-flex items-center py-2 px-3 border border-gray-300 rounded-md text-sm leading-4 font-medium text-gray-700 hover:text-gray-500 rounded-md shadow-sm active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out">
+                                            <svg class="h-5 w-5 mr-1.5" fill="none" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M2.667 10.667v.666a2 2 0 0 0 2 2h6.666a2 2 0 0 0 2-2v-.666M10.667 8L8 10.667m0 0L5.333 8M8 10.667v-8" stroke="#374151" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                            </svg>
+                                            {{ __('Upload') }}
+                                        </label>
+                                    </span>
+                                </div>
+                                @if($credentials_json)
+                                    <a class="ml-4 text-sm leading-5 text-gray-500 underline hover:text-gray-400" href="{{ \Illuminate\Support\Facades\Storage::url('analytics/service-account-credentials.json') }}">{{ __('View json.') }}</a>
+                                @endif
+                                @if(! $json_file && ! $credentials_json)
+                                    <span class="ml-4 text-sm leading-5 text-gray-400">
+                                        {{ __("No json file added.") }}
+                                    </span>
+                                @endif
                             </div>
                         </div>
                     </div>
--- src/Http/Livewire/Settings/Analytics.php
+++ src/Http/Livewire/Settings/Analytics.php
@@ -2,12 +2,17 @@
 
 namespace Shopper\Framework\Http\Livewire\Settings;
 
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 use Illuminate\Support\Facades\Artisan;
+use Livewire\WithFileUploads;
 use Shopper\Framework\Models\System\Setting;
 
 class Analytics extends Component
 {
+    use WithFileUploads;
+
     /**
      * Google Analytics tracking iD.
      *
@@ -44,6 +49,20 @@ class Analytics extends Component
     public $facebook_pixel_account_id;
 
     /**
+     * Json Credential file.
+     *
+     * @var mixed
+     */
+    public $json_file;
+
+    /**
+     * Analytics credentials.
+     *
+     * @var bool
+     */
+    public $credentials_json = false;
+
+    /**
      * Component Mount Method.
      *
      * @return void
@@ -51,11 +70,12 @@ class Analytics extends Component
     public function mount()
     {
         $ga_add_js = Setting::query()->where('key', 'google_analytics_add_js')->first();
-        $this->google_analytics_tracking_id = env('GOOGLE_ANALYTICS_TRACKING_ID');
-        $this->google_analytics_view_id = env('GOOGLE_ANALYTICS_VIEW_ID');
+        $this->google_analytics_tracking_id = env('ANALYTICS_TRACKING_ID');
+        $this->google_analytics_view_id = env('ANALYTICS_VIEW_ID');
         $this->google_analytics_add_js = $ga_add_js->value ?? null;
         $this->google_tag_manager_account_id = env('GOOGLE_TAG_MANAGER_ACCOUNT_ID');
         $this->facebook_pixel_account_id = env('FACEBOOK_PIXEL_ACCOUNT_ID');
+        $this->credentials_json = File::exists(storage_path('app/analytics/service-account-credentials.json'));
     }
 
     /**
@@ -66,8 +86,8 @@ class Analytics extends Component
     public function store()
     {
         setEnvironmentValue([
-            'google_analytics_tracking_id'  => $this->google_analytics_tracking_id,
-            'google_analytics_view_id'      => $this->google_analytics_view_id,
+            'analytics_tracking_id'         => $this->google_analytics_tracking_id,
+            'analytics_view_id'             => $this->google_analytics_view_id,
             'google_tag_manager_account_id' => $this->google_tag_manager_account_id,
             'facebook_pixel_account_id'     => $this->facebook_pixel_account_id,
         ]);
@@ -79,12 +99,22 @@ class Analytics extends Component
             'display_name' => Setting::lockedAttributesDisplayName('google_analytics_add_js'),
         ]);
 
+        if ($this->json_file) {
+            $this->json_file->storeAs('analytics', 'service-account-credentials.json');
+        }
+
         Artisan::call('config:clear');
 
         $this->notify([
             'title' => __('Updated'),
             'message' => __("Your analytics configurations have been correctly updated")
         ]);
+    }
+
+    public function downloadJson()
+    {
+        //storage_path('app/analytics/service-account-credentials.json')
+        return Storage::url('app/analytics/service-account-credentials.json');
     }
 
     /**
