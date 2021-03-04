33499ee96ae12277c3d613a343c89b889ff9ecb1
Arthur Monney <monneylobe@gmail.com>
2021-03-04 08:51:51 +0100

    :fire: remove notifications button

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -87,12 +87,6 @@
                         </div>
                     </div>
                     <div class="ml-4 flex items-center md:ml-6">
-                        <button class="p-1 text-gray-400 rounded-full hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:shadow-outline focus:text-gray-500" aria-label="Notifications">
-                            <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"/>
-                            </svg>
-                        </button>
-
                         <!-- Profile dropdown -->
                         <livewire:shopper-account.dropdown />
                     </div>
