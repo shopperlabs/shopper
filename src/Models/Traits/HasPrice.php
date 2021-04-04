9436f7dc46bc0c9e3ade1777387701d0e2772ee5
Jesper Jacobsen <jesper@oestertoft.dk>
2021-04-04 12:20:38 +0200

    Fix price being displayed in minor units but stored in major units

--- src/Models/Traits/HasPrice.php
+++ src/Models/Traits/HasPrice.php
@@ -2,25 +2,27 @@
 
 namespace Shopper\Framework\Models\Traits;
 
-use Money\Currencies\ISOCurrencies;
-use Money\Currency;
-use Money\Formatter\IntlMoneyFormatter;
 use Money\Money;
+use Money\Currency;
+use NumberFormatter;
+use Money\Currencies\ISOCurrencies;
+use Money\Formatter\IntlMoneyFormatter;
 
 trait HasPrice
 {
     /**
      * Return formatted price.
      *
-     * @param  int|string  $price
+     * @param int|string $price
+     *
      * @return bool|string
      */
     public function formattedPrice($price)
     {
-        $money = new Money($price, new Currency(shopper_currency()));
+        $money = new Money($price * 100, new Currency(shopper_currency()));
         $currencies = new ISOCurrencies();
 
-        $numberFormatter = new \NumberFormatter(app()->getLocale(), \NumberFormatter::CURRENCY);
+        $numberFormatter = new NumberFormatter(app()->getLocale(), NumberFormatter::CURRENCY);
         $moneyFormatter = new IntlMoneyFormatter($numberFormatter, $currencies);
 
         return $moneyFormatter->format($money);
