e07e20109aeda20ef10460b7f7c7b74a95c9e32d
Arthur Monney <monneylobe@gmail.com>
2021-05-08 15:38:45 +0100

    :recycle: Add modal custom parameters

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,6 +1,7 @@
 @props([
     'formAction' => false,
     'headerClasses' => '',
+    'contentClasses' => 'bg-white px-4 sm:p-6',
     'footerClasses' => 'bg-white px-4 pb-5 sm:px-4 sm:flex',
 ])
 
@@ -10,7 +11,7 @@
     @endif
         <div class="{{ $headerClasses }}">
             @if(isset($title))
-                <h3 class="text-lg leading-6 font-medium text-gray-900">
+                <h3 class="flex items-center text-lg leading-6 font-medium text-gray-900">
                     {{ $title }}
                 </h3>
             @endif
@@ -25,7 +26,7 @@
         </div>
 
         @if(isset($content))
-            <div class="bg-white px-4 sm:p-6">
+            <div class="{{ $contentClasses }}">
                 {{ $content }}
             </div>
         @endif
