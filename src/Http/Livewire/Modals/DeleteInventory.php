0be711aa2d07d5fe2d5b1c4ce262357a9be40ca2
Arthur Monney <monneylobe@gmail.com>
2021-05-18 21:27:52 +0100

    :sparkles: Add inventory delete modal feature

---
+++ src/Http/Livewire/Modals/DeleteInventory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Shopper\Framework\Http\Livewire\Modals;
+
+use LivewireUI\Modal\ModalComponent;
+use Shopper\Framework\Models\Shop\Inventory\Inventory;
+
+class DeleteInventory extends ModalComponent
+{
+    public int $inventoryId;
+    public string $name;
+
+    public function mount(int $inventoryId, string $name)
+    {
+        $this->inventoryId = $inventoryId;
+        $this->name = $name;
+    }
+
+    public function delete()
+    {
+        Inventory::query()->find($this->inventoryId)->delete();
+
+        session()->flash('success', __('Inventory Successfully removed.'));
+
+        $this->redirectRoute('shopper.settings.inventories.index');
+    }
+
+    public function render()
+    {
+        return view('shopper::livewire.modals.delete-inventory');
+    }
+}
