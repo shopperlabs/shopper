b9e0fbbeae59540d9140be60cd254ca5304c8cf1
Arthur Monney <monneylobe@gmail.com>
2021-05-28 23:38:56 +0100

    :sparkles: Add dark mode

--- resources/views/components/input/drag-upload.blade.php
+++ resources/views/components/input/drag-upload.blade.php
@@ -16,13 +16,13 @@
         </div>
     @else
         <div class="w-full">
-            <label for="{{ $attributes['id'] }}" :class="{ 'border-blue-200 bg-blue-50': focused }" class="group flex justify-center border-2 border-gray-300 dark:border-gray-700 border-dashed hover:border-blue-200 dark:hover:border-blue-500 hover:bg-blue-50 rounded-md cursor-pointer">
+            <label for="{{ $attributes['id'] }}" :class="{ 'border-blue-200 bg-blue-50': focused }" class="group flex justify-center border-2 border-gray-300 dark:border-gray-700 border-dashed hover:border-blue-200 dark:hover:border-blue-500 hover:bg-blue-50 dark:hover:bg-gray-900 rounded-md cursor-pointer">
                 <div class="px-6 pt-5 pb-6 text-center">
                     <svg class="mx-auto h-12 w-12 text-gray-400 group-hover:text-blue-500" :class="{ 'text-blue-500': focused }" stroke="currentColor" fill="none" viewBox="0 0 48 48">
                         <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                     </svg>
-                    <p class="mt-1 text-sm text-gray-600 dark:text-gray-500 group-hover:text-blue-500">
-                        <span :class="{ 'text-blue-800': focused }" class="font-medium text-blue-600 group-hover:text-blue-800 focus:outline-none focus:underline transition duration-150 ease-in-out">
+                    <p class="mt-1 text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-500">
+                        <span :class="{ 'text-blue-700': focused }" class="font-medium text-blue-600 group-hover:text-blue-700 focus:underline">
                             {{ __('Upload a file') }}
                         </span>
                         {{ __('or drag and drop') }}
--- resources/views/components/notify.blade.php
+++ resources/views/components/notify.blade.php
@@ -17,25 +17,21 @@
             x-transition:leave="transition ease-in duration-100"
             x-transition:leave-start="opacity-100"
             x-transition:leave-end="opacity-0"
-            class="max-w-sm w-full bg-white shadow-lg rounded-lg pointer-events-auto"
+            class="max-w-sm w-full bg-white dark:bg-gray-800 shadow-lg rounded-lg pointer-events-auto"
         >
             <div class="rounded-lg shadow-xs overflow-hidden">
                 <div class="p-4">
                     <div class="flex items-start">
                         <div class="flex-shrink-0">
-                            <svg class="h-6 w-6 text-green-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
-                            </svg>
+                            <x-heroicon-o-check-circle class="h-6 w-6 text-green-400" />
                         </div>
                         <div class="ml-3 w-0 flex-1 pt-0.5">
-                            <p x-text="title" class="text-sm leading-5 font-medium text-gray-900"></p>
-                            <p x-text="message" class="mt-1 text-sm leading-5 font-medium text-gray-500"></p>
+                            <p x-text="title" class="text-sm leading-5 font-medium text-gray-900 dark:text-white"></p>
+                            <p x-text="message" class="mt-1 text-sm leading-5 font-medium text-gray-500 dark:text-gray-400"></p>
                         </div>
                         <div class="ml-4 flex-shrink-0 flex">
-                            <button @click="remove(message)" class="inline-flex text-gray-400 focus:outline-none focus:text-gray-500 transition ease-in-out duration-150">
-                                <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
+                            <button @click="remove(message)" class="inline-flex text-gray-400 focus:outline-none focus:text-gray-500">
+                                <x-heroicon-s-x class="h-5 w-5" />
                             </button>
                         </div>
                     </div>
