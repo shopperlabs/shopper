3dd9815aef61249b6f91fc66fa8744f30cd1b0ee
Arthur Monney <monneylobe@gmail.com>
2021-06-09 16:52:14 +0100

    :sparkles: Add dark mode

--- resources/views/livewire/collections/products.blade.php
+++ resources/views/livewire/collections/products.blade.php
@@ -30,7 +30,7 @@
         </div>
         <div class="border-t border-gray-200 p-4 sm:px-6 sm:py-5 dark:border-gray-700">
             @if($products->isNotEmpty())
-                <div class="divide-y divide-gray-200 dark:border-gray-700">
+                <div class="divide-y divide-gray-200 dark:divide-gray-700">
                     @foreach($products as $product)
                         <div class="flex items-center justify-between py-2">
                             <div class="flex items-center">
@@ -39,13 +39,13 @@
                                         <img class="object-cover object-center w-full h-full block" src="{{ $product->getFirstImage()->file_path }}" alt="" />
                                     </span>
                                 @else
-                                    <span class="flex items-center justify-center h-10 w-10 bg-gray-100 text-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-500">
-                                        <svg class="w-8 h-8" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
+                                    <span class="flex items-center justify-center h-10 w-10 bg-gray-100 text-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-400">
+                                        <svg class="w-6 h-6" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
                                             <path d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                                         </svg>
                                     </span>
                                 @endif
-                                <p class="ml-4 text-sm font-medium text-gray-500 dark:text-gray-500">
+                                <p class="ml-4 text-sm font-medium text-gray-500 dark:text-gray-400">
                                     {{ $product->name }}
                                 </p>
                             </div>
