681c98167fddf65895889855241866c540a9357b
Arthur Monney <monneylobe@gmail.com>
2021-08-21 14:47:47 +0100

    :bento: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,7 +38,7 @@ module.exports = {
         '(screen-content)': 'calc(100vh - 9.625rem)',
       },
       fontFamily: {
-        sans: ['Manrope', ...defaultTheme.fontFamily.sans]
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
       }
     },
   },
