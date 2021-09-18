40dc1f90a7b0aea1760849adcdce8edfb5b3908a
Arthur Monney <monneylobe@gmail.com>
2021-09-18 01:30:04 +0100

    :recycle: refactoring

--- resources/views/livewire/settings/general.blade.php
+++ resources/views/livewire/settings/general.blade.php
@@ -148,33 +148,11 @@
                                                 {{ __('Remove') }}
                                             </button>
                                         </div>
-<<<<<<< HEAD
-                                    @else
-                                        <div class="w-full">
-                                            <label for="shop_cover" class="group mt-2 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md cursor-pointer">
-                                                <div class="text-center">
-                                                    <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48">
-                                                        <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                                                    </svg>
-                                                    <p class="mt-1 text-sm text-gray-600">
-                                                        <span class="font-medium text-blue-600 group-hover:text-blue-500 focus:outline-none focus:underline transition duration-150 ease-in-out">
-                                                            {{ __("Upload a file") }}
-                                                        </span>
-                                                        {{ __("or drag and drop") }}
-                                                    </p>
-                                                    <p class="mt-1 text-xs text-gray-500">
-                                                        {{ __('PNG, JPG, GIF up to 1MB') }}
-                                                    </p>
-                                                    <input id="shop_cover" type="file" wire:model="shop_cover" class="sr-only">
-                                                </div>
-                                            </label>
-=======
                                     </div>
                                 @elseif($cover)
                                     <div>
                                         <div class="shadow flex-shrink-0 rounded-md overflow-hidden">
                                             <img class="h-40 w-full object-cover" src="{{ shopper_asset($cover) }}" alt="Store Cover image">
->>>>>>> main
                                         </div>
                                         <div class="mt-1 flex items-center">
                                             <button wire:click="deleteCover" type="button" class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs leading-4 font-medium rounded text-red-700 bg-red-100 hover:bg-red-50 focus:outline-none focus:border-red-300 focus:shadow-outline-red active:bg-red-200 transition ease-in-out duration-150">
@@ -227,9 +205,9 @@
         <div class="md:grid md:grid-cols-3 md:gap-6">
             <div class="md:col-span-1">
                 <div class="px-4 sm:px-0">
-                    <h3 class="text-lg font-semibold leading-6 text-gray-900 dark:text-white">{{ __("Store address") }}</h3>
+                    <h3 class="text-lg font-semibold leading-6 text-gray-900 dark:text-white">{{ __('Store address') }}</h3>
                     <p class="mt-4 text-sm leading-5 text-gray-500">
-                        {{ __("This address will appear on your invoices. You can edit the address used.") }}
+                        {{ __('This address will appear on your invoices. You can edit the address used.') }}
                     </p>
                 </div>
             </div>
@@ -289,7 +267,7 @@
         <div class="md:grid md:grid-cols-3 md:gap-6">
             <div class="md:col-span-1">
                 <div class="px-4 sm:px-0">
-                    <h3 class="text-lg font-semibold leading-6 text-gray-900 dark:text-white">{{ __("Store currency") }}</h3>
+                    <h3 class="text-lg font-semibold leading-6 text-gray-900 dark:text-white">{{ __('Store currency') }}</h3>
                     <p class="mt-4 text-sm leading-5 text-gray-500">
                         {{ __("This is the currency your products are sold in. After your first sale, currency is locked in and can’t be changed.") }}
                     </p>
