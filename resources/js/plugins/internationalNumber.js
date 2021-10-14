6842cf79a3473e5174f7ea22431d9817bf4f60b6
Arthur Monney <monneylobe@gmail.com>
2021-10-14 12:29:28 +0100

    :bento: Add alpine data plugin for international number

---
+++ resources/js/plugins/internationalNumber.js
@@ -0,0 +1,26 @@
+import intlTelInput from 'intl-tel-input';
+
+export default (element) => ({
+  input: element, // '#myID' selector css
+
+  init() {
+    const phoneNumber = document.querySelector(this.input);
+    let iti = intlTelInput(phoneNumber, {
+      nationalMode: true,
+      geoIpLookup: function(success, failure) {
+        fetch('https://ipinfo.io').then(response => {
+          let countryCode = (response && response.country) ? response.country : 'CM';
+          success(countryCode);
+        })
+      },
+      utilsScript: 'https://unpkg.com/intl-tel-input@17.0.3/build/js/utils.js'
+    });
+    let handleChange = () => {
+      if (iti.isValidNumber()) {
+        phoneNumber.value = iti.getNumber();
+      }
+    };
+    phoneNumber.addEventListener('change', handleChange);
+    phoneNumber.addEventListener('keyup', handleChange);
+  }
+})
