a795e7e2908ab007c5c462caca37b3f9dfad54ff
Arthur Monney <monneylobe@gmail.com>
2021-11-13 20:24:13 +0100

    update colors

--- resources/views/sidebar/group.blade.php
+++ resources/views/sidebar/group.blade.php
@@ -1,6 +1,6 @@
 <div>
     @if($group->shouldShowHeading())
-        <h5 class="menu-heading text-xs leading-4 text-primary-800 dark:text-primary-600 uppercase tracking-wider mb-2 font-semibold ml-5">{{ $group->getName() }}</h5>
+        <h5 class="menu-heading text-xs leading-4 text-primary-700 uppercase tracking-wider mb-2 font-semibold ml-5">{{ $group->getName() }}</h5>
     @endif
 
     @foreach($items as $item)
