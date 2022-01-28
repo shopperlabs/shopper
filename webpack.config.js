998482d70d40d1f2199452db3861f936e1b7c9cd
Arthur Monney <monneylobe@gmail.com>
2022-01-28 22:50:30 +0100

    :arrow_up: upgrade to Tailwind v3

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,16 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
   output: { chunkFilename: 'js[name].js?id=[chunkhash]' },
   resolve: {
     extensions: ['*', '.js', '.jsx'],
     alias: {
-      '@': path.resolve('resources/js'),
+      '@': path.resolve('./resources/js'),
       '@api': path.resolve('./resources/js/api'),
       '@components': path.resolve('./resources/js/components'),
       '@helpers': path.resolve('./resources/js/helpers'),
+      'react': 'preact/compat',
+      'react-dom': 'preact/compat'
     },
-    modules: [
-      'node_modules',
-      __dirname + '/vendor/spatie/laravel-medialibrary-pro/resources/js',
-    ]
   },
-};
+}
