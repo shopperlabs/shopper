5f2050cd48aa01b162949a55cbe9b3331f33f960
Arthur Monney <monneylobe@gmail.com>
2022-04-08 05:18:17 +0100

    :lipstick: update app layout

--- resources/views/components/layouts/app/sidebar/index.blade.php
+++ resources/views/components/layouts/app/sidebar/index.blade.php
@@ -1,7 +1,9 @@
 <aside class="hidden md:flex md:shrink-0">
-    <div class="flex w-72 bg-white dark:bg-secondary-900 border-r border-transparent md:border-secondary-200 md:dark:border-secondary-700">
+    <div class="flex">
         <x-shopper::layouts.app.sidebar.primary />
 
-        <x-shopper::layouts.app.sidebar.secondary />
+        <div class="w-56 bg-white dark:bg-secondary-900 border-r border-transparent md:border-secondary-200 md:dark:border-secondary-700">
+            <x-shopper::layouts.app.sidebar.secondary />
+        </div>
     </div>
 </aside>
