b107a3452b04bea3cacb32452cb4b67eb81ccb63
Özkan Özcan <grafikerozkan@gmail.com>
2022-04-13 20:46:04 +0300

    Turkish translation completed

---
+++ resources/lang/tr/messages.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Messages Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the shopper package.
+    | You are free to change them to anything you want to customize your
+    | views to better match your application.
+    |
+    */
+
+    'discount_use' => '{1} Limitten 1 tanesi kullanıldı|[2,*] Limitten :count tanesi kullanıldı',
+
+];
