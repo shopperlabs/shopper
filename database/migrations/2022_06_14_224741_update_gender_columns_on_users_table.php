e0c428bdff8734e0ad59dcf3445e1e596c20ecfe
Arthur Monney <monneylobe@gmail.com>
2022-06-15 00:02:20 +0100

    :card_file_box: chnage gender column type from enum to string with nullable and default value

---
+++ database/migrations/2022_06_14_224741_update_gender_columns_on_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Shopper\Framework\Traits\Database;
+
+return new class extends Migration
+{
+    use Database\Migration;
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table($this->getTableName('users'), function (Blueprint $table) {
+            $table->string('gender')->nullable()->default('male')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
