73d9a10078f13ae538f7c00b4ae63e7fd99e5ed1
Arthur Monney <monneylobe@gmail.com>
2022-06-23 07:45:36 +0100

    add brands api for create select search input

--- src/Http/Controllers/Api/BrandController.php
+++ src/Http/Controllers/Api/BrandController.php
@@ -2,9 +2,32 @@
 
 namespace Shopper\Framework\Http\Controllers\Api;
 
-class BrandController{
-    public function __invoke()
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Request;
+use Shopper\Framework\Http\Controllers\ShopperBaseController;
+use Shopper\Framework\Repositories\Ecommerce\BrandRepository;
+
+class BrandController extends ShopperBaseController
+{
+    public function __invoke(Request $request): Collection
     {
-        //
+        return (new BrandRepository())
+            ->makeModel()
+            ->newQuery()
+            ->select('id', 'name')
+            ->orderBy('name')
+            ->when(
+                $request->search,
+                fn (Builder $query) => $query
+                    ->where('name', 'like', "%{$request->search}%")
+            )
+            ->when(
+                $request->exists('selected'),
+                fn (Builder $query) => $query->whereIn('id', $request->selected),
+                fn (Builder $query) => $query->limit(10)
+            )
+            ->get();
+
     }
 }
