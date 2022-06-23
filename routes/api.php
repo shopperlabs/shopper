2f15ec911f6240c8d2e9a821836f9c6c81c0dbc0
Arthur Monney <monneylobe@gmail.com>
2022-06-23 07:44:17 +0100

    add api routes for admin panel

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,14 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+use Shopper\Framework\Http\Controllers\Api;
+
+/*
+|--------------------------------------------------------------------------
+| Backend API Routes
+|--------------------------------------------------------------------------
+|
+|
+*/
+
+Route::get('/brands', Api\BrandController::class)->name('brands');
