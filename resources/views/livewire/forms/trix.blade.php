d2eb9aa06ed2115e3f9c3faeab3f867f141c8151
Arthur Monney <monneylobe@gmail.com>
2023-04-14 02:02:36 +0200

    wip

--- resources/views/livewire/forms/trix.blade.php
+++ resources/views/livewire/forms/trix.blade.php
@@ -1,7 +1,3 @@
-@push('styles')
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.min.css" />
-@endpush
-
 <div
     wire:ignore
     x-data
@@ -10,7 +6,3 @@
     <input id="{{ $trixId }}" value="{{ $value }}" type="hidden" class="sr-only">
     <trix-editor input="{{ $trixId }}" class="block w-full rounded-md shadow-sm border-secondary-300 dark:bg-secondary-700 dark:text-white dark:border-secondary-700 focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 sm:text-sm max-h-96 overflow-y-scroll"></trix-editor>
 </div>
-
-@push('scripts')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.min.js"></script>
-@endpush
