421a720de89f4595e76561682776eb65a6462782
Arthur Monney <monneylobe@gmail.com>
2023-05-06 02:48:55 +0200

    :coffin: Remove Brand api controller

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -17,58 +17,25 @@ class RouteServiceProvider extends ServiceProvider
 
     public function map(): void
     {
-        $this->configureRateLimiting();
+        $this->routes(function () {
+            Route::namespace($this->namespace . '\Auth')
+                ->middleware('web')
+                ->as('shopper.')
+                ->prefix(Shopper::prefix())
+                ->group(realpath(SHOPPER_PATH . '/routes/auth.php'));
 
-        $this->mapApiRoutes();
-
-        $this->mapAuthRoutes();
-
-        $this->mapBackendRoutes();
-
-        $this->mapCustomBackendRoute();
-    }
-
-    public function mapApiRoutes(): void
-    {
-        Route::namespace($this->namespace . '\Api')
-            ->middleware('api')
-            ->as('shopper.api.')
-            ->prefix(Shopper::prefix() . '/api')
-            ->group(realpath(SHOPPER_PATH . '/routes/api.php'));
-    }
-
-    protected function mapAuthRoutes(): void
-    {
-        Route::namespace($this->namespace . '\Auth')
-            ->middleware('web')
-            ->as('shopper.')
-            ->prefix(Shopper::prefix())
-            ->group(realpath(SHOPPER_PATH . '/routes/auth.php'));
-    }
-
-    protected function mapBackendRoutes(): void
-    {
-        Route::middleware(['shopper', 'dashboard'])
-            ->prefix(Shopper::prefix())
-            ->as('shopper.')
-            ->namespace($this->namespace)
-            ->group(realpath(SHOPPER_PATH . '/routes/backend.php'));
-    }
-
-    public function mapCustomBackendRoute(): void
-    {
-        if (config('shopper.routes.custom_file')) {
             Route::middleware(['shopper', 'dashboard'])
                 ->prefix(Shopper::prefix())
-                ->namespace(config('shopper.system.controllers.namespace'))
-                ->group(config('shopper.routes.custom_file'));
-        }
-    }
+                ->as('shopper.')
+                ->namespace($this->namespace)
+                ->group(realpath(SHOPPER_PATH . '/routes/backend.php'));
 
-    protected function configureRateLimiting(): void
-    {
-        RateLimiter::for('shopper.api', function (Request $request) {
-            return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
+            if (config('shopper.routes.custom_file')) {
+                Route::middleware(['shopper', 'dashboard'])
+                    ->prefix(Shopper::prefix())
+                    ->namespace(config('shopper.system.controllers.namespace'))
+                    ->group(config('shopper.routes.custom_file'));
+            }
         });
     }
 }
