4f7687205f2f258721be7b7a9356000f59b7a710
Arthur Monney <monneylobe@gmail.com>
2023-05-10 11:19:46 +0200

    :bug: Fix routes not Found

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -14,25 +14,38 @@ class RouteServiceProvider extends ServiceProvider
 
     public function map(): void
     {
-        $this->routes(function () {
-            Route::namespace($this->namespace . '\Auth')
-                ->middleware('web')
-                ->as('shopper.')
-                ->prefix(Shopper::prefix())
-                ->group(realpath(SHOPPER_PATH . '/routes/auth.php'));
+        $this->mapAuthRoutes();
 
+        $this->mapBackendRoutes();
+
+        $this->mapCustomBackendRoute();
+    }
+
+    protected function mapAuthRoutes(): void
+    {
+        Route::namespace($this->namespace . '\Auth')
+            ->middleware('web')
+            ->as('shopper.')
+            ->prefix(Shopper::prefix())
+            ->group(realpath(SHOPPER_PATH . '/routes/auth.php'));
+    }
+
+    protected function mapBackendRoutes(): void
+    {
+        Route::middleware(['shopper', 'dashboard'])
+            ->prefix(Shopper::prefix())
+            ->as('shopper.')
+            ->namespace($this->namespace)
+            ->group(realpath(SHOPPER_PATH . '/routes/backend.php'));
+    }
+
+    public function mapCustomBackendRoute(): void
+    {
+        if (config('shopper.routes.custom_file')) {
             Route::middleware(['shopper', 'dashboard'])
                 ->prefix(Shopper::prefix())
-                ->as('shopper.')
-                ->namespace($this->namespace)
-                ->group(realpath(SHOPPER_PATH . '/routes/backend.php'));
-
-            if (config('shopper.routes.custom_file')) {
-                Route::middleware(['shopper', 'dashboard'])
-                    ->prefix(Shopper::prefix())
-                    ->namespace(config('shopper.system.controllers.namespace'))
-                    ->group(config('shopper.routes.custom_file'));
-            }
-        });
+                ->namespace(config('shopper.system.controllers.namespace'))
+                ->group(config('shopper.routes.custom_file'));
+        }
     }
 }
