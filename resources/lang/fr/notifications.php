4e3ab95c46c4a9ee1e936fe221304d36cc20f79e
Arthur Monney <monneylobe@gmail.com>
2023-05-15 06:03:53 +0200

    Add new localization files

---
+++ resources/lang/fr/notifications.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+    'users_roles' => [
+        'role_added' => 'Un nouveau rôle a été ajouté avec succès',
+        'admin_deleted' => 'Admin supprimé avec succès',
+    ],
+
+];
