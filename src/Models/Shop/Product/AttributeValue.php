7885ccc76f977a7e5083744791b2e59d3c77d5e3
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2023-06-13 11:17:57 +0100

    imported the return relationship classes

--- src/Models/Shop/Product/AttributeValue.php
+++ src/Models/Shop/Product/AttributeValue.php
@@ -7,6 +7,7 @@ namespace Shopper\Framework\Models\Shop\Product;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 /**
  * @property string $key
