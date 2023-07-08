8b7cfe8adeed365d412fd0a1e31faae3e6e6d55b
Arthur Monney <monneylobe@gmail.com>
2023-07-08 23:49:55 +0200

    Add Authenticate trait

---
+++ packages/admin/src/Traits/HasAuthenticated.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Shopper\Traits;
+
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Shopper\Facades\Shopper;
+
+trait HasAuthenticated
+{
+    public function getUser(): Model | Authenticatable | null
+    {
+        return Shopper::auth()->user();
+    }
+}
