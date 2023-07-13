b27047173887d181210c83fad5350124a733d0ce
mckenziearts <mckenziearts@users.noreply.github.com>
2023-07-13 03:18:28 +0000

    fix code formatting

--- packages/core/src/Models/Product.php
+++ packages/core/src/Models/Product.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Shopper\Core\Models;
 
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
