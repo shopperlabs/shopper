126aff11df2a892db979378e733cfb620f832b47
Arthur Monney <monneylobe@gmail.com>
2023-07-24 01:41:13 +0200

    Fix analytics

--- packages/admin/src/Http/Responses/LoginResponse.php
+++ packages/admin/src/Http/Responses/LoginResponse.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace Shopper\Http\Responses;
 
+use Illuminate\Http\RedirectResponse;
 use Livewire\Redirector;
 use Shopper\Contracts\LoginResponse as Responsable;
 
 final class LoginResponse implements Responsable
 {
-    public function toResponse($request): Redirector
+    public function toResponse($request): Redirector | RedirectResponse
     {
         return redirect()->intended(route('shopper.dashboard'));
     }
