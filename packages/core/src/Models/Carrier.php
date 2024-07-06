0b95a695b3bcea6d6bf88fb7edc7d7f32c76c9fc
mckenziearts <mckenziearts@users.noreply.github.com>
2024-07-06 17:19:52 +0000

    fix code formatting

--- packages/core/src/Models/Carrier.php
+++ packages/core/src/Models/Carrier.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Shopper\Core\Traits\HasSlug;
 use Shopper\Core\Traits\HasZones;
 
