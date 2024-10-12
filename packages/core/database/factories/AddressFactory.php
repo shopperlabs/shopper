a24ed43340eedf29aa2e93a4fd8024ac7c331d34
Stevy Endaman <stevyabessolo@gmail.com>
2024-10-12 20:00:02 +0200

    fix: change addresfactory remove country_id and change postcode to postal_code

--- packages/core/database/factories/AddressFactory.php
+++ packages/core/database/factories/AddressFactory.php
@@ -24,14 +24,13 @@ final class AddressFactory extends Factory
     public function definition(): array
     {
         return [
-            'country_id' => Country::factory(),
             'first_name' => $this->faker->firstName(),
             'last_name' => $this->faker->lastName(),
             'company_name' => $this->faker->company(),
             'street_address' => $this->faker->streetAddress(),
             'street_address_plus' => $this->faker->streetSuffix(),
             'city' => $this->faker->city(),
-            'postcode' => $this->faker->postcode(),
+            'postal_code' => $this->faker->postcode(),
             'type' => $this->faker->randomElement(AddressType::values()),
             'shipping_default' => $this->faker->boolean,
             'billing_default' => $this->faker->boolean,
