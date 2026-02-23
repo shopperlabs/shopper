7e10e3892f60c8d4326966dd99bc5f1375e3027c
Arthur Monney <monneylobe@gmail.com>
2026-02-23 22:02:27 +0100

    fix: add PHPStan ignore rules for currencyMask and withTrashed methods

--- packages/admin/src/Components/Form/CustomAttributeKeyInput.php
+++ packages/admin/src/Components/Form/CustomAttributeKeyInput.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 namespace Shopper\Components\Form;
 
 use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\Field;
 use Filament\Forms\Components\TextInput;
-use Filament\Support\Components\Component;
 use Shopper\Core\Enum\FieldType;
 
 final class CustomAttributeKeyInput
 {
-    public static function make(string $key, FieldType $type): Component
+    public static function make(string $key, FieldType $type): Field
     {
         return match ($type) {
             FieldType::ColorPicker => ColorPicker::make($key)
