cbca93663f629cda10249d94cd79a3d81e796001
Arthur Monney <monneylobe@gmail.com>
2026-03-11 22:35:19 +0100

    fix: use filament icon component in icon column to support all icon providers

--- packages/admin/resources/views/filament/tables/icon-column.blade.php
+++ packages/admin/resources/views/filament/tables/icon-column.blade.php
@@ -1,5 +1,5 @@
 <div class="filament-icon-picker-icon-column px-4 py-3">
     @if ($icon = $getState())
-        <x-icon class="size-6 text-gray-500 dark:text-gray-400" :name="$icon" aria-hidden="true" />
+        <x-filament::icon :$icon />
     @endif
 </div>
