23a19748e4bc05fd5a210e1df20ee3f70a616b57
Arthur Monney <monneylobe@gmail.com>
2026-03-11 15:33:33 +0100

    feat: add branded 403 forbidden page with redirect-based exception handling

---
+++ packages/admin/resources/lang/en/errors.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+    '403' => [
+        'title' => 'Access Forbidden',
+        'description' => 'You don\'t have permission to access this page. Contact your administrator if you believe this is a mistake.',
+        'back' => 'Back to Dashboard',
+        'go_back' => 'Go back',
+    ],
+
+];
---
+++ packages/admin/resources/lang/es/errors.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+    '403' => [
+        'title' => 'Acceso denegado',
+        'description' => 'No tienes permiso para acceder a esta página. Contacta a tu administrador si crees que esto es un error.',
+        'back' => 'Volver al panel',
+        'go_back' => 'Volver atrás',
+    ],
+
+];
