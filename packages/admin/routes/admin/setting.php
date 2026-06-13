29707aafd8c4018c3b32d9fb126bf946d3f3a34d
Arthur Monney <monneylobe@gmail.com>
2026-06-13 17:24:49 +0200

    refactor(admin): link settings directly to general page (#550)
    Point the settings sidebar item and breadcrumb root to the general
    page and remove the intermediate settings index page along with its
    route, config entry, view, dead menu component, render hooks and test.

--- packages/admin/routes/admin/setting.php
+++ packages/admin/routes/admin/setting.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', config('shopper.components.setting.pages.setting-index'))->name('index');
 Route::get('/general', config('shopper.components.setting.pages.general'))->name('shop');
 Route::get('/appearance', config('shopper.components.setting.pages.appearance'))->name('appearance');
 
